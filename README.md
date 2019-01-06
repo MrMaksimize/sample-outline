2793031afabc06144ad167f5f35464c62223b9eb
Maksim P <max@maksimize.com>
2019-01-06 12:25:32 -0800

    Update outline links, add gitignore.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Course Outline, Answers, Exercise
-https://docs.google.com/document/d/15D60-RrKvvohAneI8m66L58Ze6rfN9RdPzKsZjJQubo/edit
+# Course Outline, Answers
+[Please see GDoc for outline](https://docs.google.com/document/d/1_FmB6nwtxNJ4Ss_yoNRlq2L_YqA-4PMRLd3h12DpKIU/edit)
+
+# Exercise
+[Please see GDoc for Exercise](https://docs.google.com/document/d/1_FmB6nwtxNJ4Ss_yoNRlq2L_YqA-4PMRLd3h12DpKIU/edit#bookmark=id.eo9w6bp3bx21)
--- exercise.py
+++ exercise.py
@@ -1,4 +1,5 @@
-import os, subprocess, datetime
+import os, subprocess
+# These Next 3 Lines are setup - so I'm assuming they don't count in the 15 line limit.
 POTHOLE_REQ = "http://seshat.datasd.org/get_it_done_311/get_it_done_pothole_requests_datasd.csv"
 GRAF_REQ = "http://seshat.datasd.org/get_it_done_311/get_it_done_graffiti_removal_requests_datasd.csv"
 DOWNLOADS_DIR = "{}/temp".format(os.getcwd())
@@ -10,16 +11,15 @@ if not (os.path.exists(DOWNLOADS_DIR)):
 
 subprocess.check_call(["wget", GRAF_REQ, "-O", "{}/graffiti_req.csv".format(DOWNLOADS_DIR)])
 subprocess.check_call(["wget", POTHOLE_REQ, "-O", "{}/pothole_req.csv".format(DOWNLOADS_DIR)])
-output = ""
-for f in os.listdir(DOWNLOADS_DIR):
-    file_is_valid = True;
-    filepath = "{}/{}".format(DOWNLOADS_DIR, f)
-    output += subprocess.check_output(["cut", "-d", ",", "-f", "1,2,4,7", filepath]).decode('utf-8')
-
 final_file = open(FINAL_FILE, 'w')
-final_file.write(output);
 
+for f in os.listdir(DOWNLOADS_DIR):
+    filepath = "{}/{}".format(DOWNLOADS_DIR, f)
+    output = subprocess.check_output(["cut", "-d", ",", "-f", "1,2,4,7", filepath]).decode('utf-8')
+    final_file.write(output);
+    os.remove(filepath)
+
+final_file.close()
 number_of_lines = subprocess.check_output(["wc", "-l", FINAL_FILE]).decode('utf-8')
-
 print("Number of lines in all requests is {}".format(number_of_lines))
 
